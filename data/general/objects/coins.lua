655e7d1f8ddae650b7ba34b8777845aeac75e776
Jim Niemira <jim.niemira@gmail.com>
2015-01-31 14:54:24 -0500

    added treasures and gems

--- data/general/objects/coins.lua
+++ data/general/objects/coins.lua
@@ -1,22 +1,3 @@
--- ToME - Tales of Maj'Eyal
--- Copyright (C) 2009 - 2014 Nicolas Casalini
---
--- This program is free software: you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation, either version 3 of the License, or
--- (at your option) any later version.
---
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the -- GNU General Public License for more details.
---
--- You should have received a copy of the GNU General Public License
--- along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
--- Nicolas Casalini "DarkGod"
--- darkgod@te4.org
-
---local Stats = require "engine.interface.ActorStats"
 
 newEntity{
 	define_as = "BASE_COIN",
@@ -26,16 +7,10 @@ newEntity{
 	use_no_wear = true, identified = true, stacking = true,
 	auto_pickup = true, pickup_sound = "actions/coin",
 	desc = [[Ooh, a shiny ...]],
---        	game.logPlayer(who, "You pickup %0.2f gold pieces.", self.money_value / 10)
---        	-- Remove from the map
---        	game.level.map:removeObject(who.x, who.y, id)
---        	return true
---    	end,
 }
 
 
 local function newCoin(name, image, subtype, cost, rarity, min_level, max_level, min_count, max_count)
-	print ("defining ", name:gsub(" ", "_"):upper())
 	newEntity{
 		base = "BASE_COIN",
 		define_as = name:gsub(" ", "_"):upper(),
---
+++ data/general/objects/treasures.lua
@@ -0,0 +1,63 @@
+
+-- Treasure - items whose only purpose is to be sold for coin
+-- Probably won't include gems, as those may be used for else?
+
+-- quality to value multiplier
+local quality = {
+	{"roughly-made", 1},
+	{"amateurish", 5},
+	{"mediocre", 10},
+	{"fine", 20},
+	{"excellent", 50},
+	{"masterpiece", 100},
+	{"legendary", 1000},
+	}
+
+-- size to encumbrance multiplier
+local size = {
+	{"tiny", 1},
+	{"small", 5},
+	{"medium", 10},
+	{"large", 100},
+	{"enormous", 1000},
+	}
+
+newEntity{
+	define_as = "BASE_TREASURE",
+	type = "treasure", --subtype="copper",
+	display = "*", color=colors.YELLOW,
+	encumber = 0,
+	use_no_wear = true, identified = true, stacking = true,
+	auto_pickup = true, pickup_sound = "actions/coin",
+	desc = [[Something valuable to sell.]],
+}
+
+
+local function newTreasure(name, image, subtype, rarity, min_level, max_level, basecost, enc)
+	newEntity{
+		base = "BASE_TREASURE",
+		define_as = name:gsub(" ", "_"):upper(),
+		name = name:lower(),
+		image = image, subtype = subtype, rarity = rarity,
+		level_range = {min_level, max_level},
+		cost = basecost,
+		encumber = enc,
+		--on_prepickup = function(self, who, id)
+			--local count = rng.range(min_count, max_count)
+			--game.logPlayer(who, "Counting %d %s", count, self.name)
+			---- Ponder - liquid vs networth?  ;-)
+			---- actual coin incs max and current, jewelry incs max only?
+			---- liquid used for bribes ...
+        		--game.player.incMaxGold(game.player, cost * count)
+        		--game.player.incGold(game.player, cost * count)
+			--game.logPlayer(who, "Added %0.2f gold", cost * count)
+        		--game.level.map:removeObject(who.x, who.y, id)
+        		--return true
+		--end
+	}
+end
+
+newTreasure("Persian rug",   "treasure/coins.png", "furniture", 1, 1, 10, 100, 50)
+newTreasure("small painting",   "treasure/coins.png", "furniture", 1, 1, 10, 100, 5)
+newTreasure("painting",   "treasure/coins.png", "furniture", 1, 1, 10, 100, 40)
+newTreasure("large painting",   "treasure/coins.png", "furniture", 1, 1, 10, 100, 100)
